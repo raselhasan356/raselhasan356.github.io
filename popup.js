4fb036c076b7ce7f6c09ab2c4d7c26ea208da7a7
maksudvivasoft <154959531+maksudvivasoft@users.noreply.github.com>
2024-03-27 14:15:10 +0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -37,14 +37,31 @@ var addEvent = function (obj, evt, fn) {
 
 
 (function() {
+  var startingTime = new Date().getTime();
+  // Load the script
+  var script = document.createElement("SCRIPT");
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+  script.type = 'text/javascript';
+  document.getElementsByTagName("head")[0].appendChild(script);
 
+  // Poll for jQuery to come into existance
+  var checkReady = function(callback) {
+      if (window.jQuery) {
+          callback(jQuery);
+      }
+      else {
+          window.setTimeout(function() { checkReady(callback); }, 20);
+      }
+  };
 
-  var foo = 3;
-  console.log(foo);
-
-  //onload();
-
-
+  // Start polling...
+  checkReady(function($) {
+      $(function() {
+          var endingTime = new Date().getTime();
+          var tookTime = endingTime - startingTime;
+          window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+      });
+  });
 })();
 
 
